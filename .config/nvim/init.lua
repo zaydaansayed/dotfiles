2b1cf1d67008d971bac729af979553c97c6c9486
zaydaansayed <zaydaansayed@icloud.com>
2026-07-12 13:25:30 +0200

    did nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,16 @@ require("config.lazy")
 
 vim.opt.termguicolors = true
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'lua' },
+  callback = function()
+    vim.treesitter.start()
+  end
+})
+
 require("nvim-tree").setup()
+require('lualine').setup()
 
 vim.keymap.set('n', 'e ', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
+vim.keymap.set('n', 'es', '<cmd>Telescope find_files<CR>', { desc = 'Open telescope search' })
+vim.keymap.set('n', 'ec', '<cmd>vert term<CR>', { desc = 'Open terminal' })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,32 @@
 return {
   "nvim-tree/nvim-tree.lua",
   "nvim-tree/nvim-web-devicons",
+  "nvim-telescope/telescope.nvim",
+  "nvim-lua/plenary.nvim",
+  "nvim-lualine/lualine.nvim",
+  "elkowar/yuck.vim",
+  "numToStr/Comment.nvim",
+  "windwp/nvim-autopairs",
+  "tpope/vim-fugitive",
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("bufferline").setup({})
+    end
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        cmdline = { view = "cmdline_popup" },
+      })
+    end
+  },
 }
